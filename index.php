da9d286938cd6170994c7b7e68fe99678f052088
unknown <A00847680@SE12L0320-05.ad.bcit.ca>
2016-01-07 11:19:50 -0800

    PHP Strings

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
- echo "hello world";
+ $temp = 'Celia. ';
+ echo 'Hi, my name is ';
+ echo $temp;
+ $temp = 'amazing. ';
+ echo " I am ";
+ echo $temp;
+ $temp = 10;
+ echo 'my awesomeness level is ';
+ echo $temp;
 
 

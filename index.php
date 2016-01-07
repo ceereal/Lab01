92225aceb092802a9a2a39a0a0d1764cddf16a12
unknown <A00847680@SE12L0320-05.ad.bcit.ca>
2016-01-07 11:26:00 -0800

    PHP Expressions

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-$name = "Jim";
-$what = "geek";
-$level = 10;
-echo 'Hi, my name is '.$name,'. and I am a level '.$level.' '.$what;
+$hoursworked = 10;
+$rate = 12;
+$total = $hoursworked * $rate;
+echo 'You owe me '.$total;
+?>
